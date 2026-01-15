c8449b06e25e38e958c6b9a297763dcd31b56108
Abr0HR <armybratofficial@gmail.com>
2026-01-15 19:04:57 +0530

    Implement React Router v6 for SPA routing - Fix direct URL access

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CompleteLandingPage from './pages/CompleteLandingPage';
 import EmployeePortalLogin from './pages/EmployeePortalLogin';
 import EmployeeRegistrationPage from './pages/EmployeeRegistrationPage';
@@ -8,60 +9,18 @@ import PasswordResetPage from './pages/PasswordResetPage';
 import './App.css';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('landing');
-  const [resetEmail, setResetEmail] = useState('');
-
-  const handleNavigate = (page, email = '') => {
-    setCurrentPage(page);
-    if (email) setResetEmail(email);
-    window.scrollTo(0, 0);
-  };
-
-  // Router logic based on current page and URL
-  React.useEffect(() => {
-    const path = window.location.pathname;
-    const params = new URLSearchParams(window.location.search);
-    
-    if (path === '/login') {
-      setCurrentPage('login');
-    } else if (path === '/register') {
-      setCurrentPage('employee-register');
-    } else if (path === '/company-register') {
-      setCurrentPage('company-register');
-    } else if (path === '/forgot-password') {
-      setCurrentPage('forgot-password');
-    } else if (path === '/reset-password') {
-      const email = params.get('email');
-      setCurrentPage('reset-password');
-      if (email) setResetEmail(email);
-    } else {
-      setCurrentPage('landing');
-    }
-  }, []);
-
-  // Render appropriate page
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'login':
-        return <EmployeePortalLogin />;
-      case 'employee-register':
-        return <EmployeeRegistrationPage />;
-      case 'company-register':
-        return <CompanyRegistrationPage />;
-      case 'forgot-password':
-        return <ForgotPasswordPage />;
-      case 'reset-password':
-        return <PasswordResetPage />;
-      case 'landing':
-      default:
-        return <CompleteLandingPage />;
-    }
-  };
-
   return (
-    <div className="App">
-      {renderPage()}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<CompleteLandingPage />} />
+        <Route path="/login" element={<EmployeePortalLogin />} />
+        <Route path="/register" element={<EmployeeRegistrationPage />} />
+        <Route path="/company-register" element={<CompanyRegistrationPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password" element={<PasswordResetPage />} />
+        <Route path="*" element={<CompleteLandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
